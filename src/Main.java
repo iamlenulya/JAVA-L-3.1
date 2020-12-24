dc74fdc9e79f26c071fb723834d3eafbc9105f29
Elena Degteva <lavishblanket@gmail.com>
2020-12-24 21:59:07 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticketPrice = 1_620;
+        int oneMile = 20;
+
+        long amount = ticketPrice / oneMile;
+        System.out.println(amount);
+    }
+}
